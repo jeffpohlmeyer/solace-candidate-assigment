cc26308843cacbab2379a7a967199d3de5aa9b22
jeffreypohlmeyer <jeffvp@protonmail.ch>
2025-08-17 09:08:44 -0400

    fix: schema.ts
    the `specialties` column was called "payload" in the database. it was also not typed properly.
    
    exporting auto-generated types based on the table from this file, to be used elsewhere

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from "drizzle-orm";
+import {InferInsertModel, InferSelectModel, sql} from "drizzle-orm";
 import {
   pgTable,
   integer,
@@ -15,10 +15,13 @@ const advocates = pgTable("advocates", {
   lastName: text("last_name").notNull(),
   city: text("city").notNull(),
   degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
+  specialties: jsonb("specialties").$type<string[]>().default([]).notNull(),
   yearsOfExperience: integer("years_of_experience").notNull(),
   phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
 });
+
+export type AdvocateType = InferSelectModel<typeof advocates>
+export type NewAdvocateType = InferInsertModel<typeof advocates>
 
 export { advocates };
