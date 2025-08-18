ed1d9ee3666f59bb87b92613ee4375153c817869
jeffreypohlmeyer <jeffvp@protonmail.ch>
2025-08-18 16:33:23 -0400

    organizing top content section to look a bit nicer and be a bit more idiomatic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,20 +42,28 @@ export default function Home() {
   };
 
   return (
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <button onClick={onClick}>Reset Search</button>
     <main className="p-6">
       <h1 className="font-serif text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
         Solace Advocates
       </h1>
+      <div className="flex items-end space-x-2 py-8">
+        <div>
+          <label className="block" htmlFor="search">
+            Search
+          </label>
+          <input
+            id="search"
             className="h-[30px] rounded-md border border-gray-200 px-1 outline-none focus-visible:ring-1 focus-visible:ring-[#285e50] focus-visible:ring-offset-2"
+            value={search}
+            onChange={onChange}
+          />
+        </div>
+        <button
+          className="rounded-md border border-[#285e50] px-2 py-1 text-sm text-[#285e50] outline-none hover:bg-[#285e50]/10 focus-visible:ring-1 focus-visible:ring-[#285e50] focus-visible:ring-offset-2"
+          onClick={handleResetSearch}
+        >
+          Reset Search
+        </button>
       </div>
       <br />
       <br />
