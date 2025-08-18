c24eaedbeab0dd4b8446d96b82138a1a5d221686
jeffreypohlmeyer <jeffvp@protonmail.ch>
2025-08-18 16:32:40 -0400

    using mollie glaston font for `h1`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,6 +52,9 @@ export default function Home() {
         </p>
         <button onClick={onClick}>Reset Search</button>
     <main className="p-6">
+      <h1 className="font-serif text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+        Solace Advocates
+      </h1>
             className="h-[30px] rounded-md border border-gray-200 px-1 outline-none focus-visible:ring-1 focus-visible:ring-[#285e50] focus-visible:ring-offset-2"
       </div>
       <br />
